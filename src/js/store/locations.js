5829ac99aa2cca0af0e49ef1e46600283dc9fa1b
AlexeevSasha <work.alexeevs@gmail.com>
2021-10-16 08:15:30 +0300

    convert to format { 'City name, Country name': {...} }

--- src/js/store/locations.js
+++ src/js/store/locations.js
@@ -15,11 +15,41 @@ class Locations {
         ]);
 
         const [countries, cities] = response;
-        this.countries = countries;
-        this.cities = cities;
+        this.countries = this.serializeCountries(countries);
+        this.cities = this.serializeCities(cities);
 
         return response;
     }
+
+
+    // [{}, {}] так выглядят наши данные с сервера
+    // { 'City, Country', null } в таком формате получает данные наш     Autocomplete;
+
+    //преобразуем в формат { 'Country-code': {...} }
+    serializeCountries(countries) {
+        return countries.reduce((acc, country) => {
+            acc[country.code] = country;
+            return acc;
+        }, {})
+    }
+
+    //преобразуем в формат { 'City name, Country name': {...} }
+    serializeCities(cities) {
+        return cities.reduce((acc, city) => {
+            const country_name = this.getCountryNameByCode(city.country_code);
+            const city_name = city.name || city.name_translations.en;
+            const key = `${city_name}, ${country_name}`
+            acc[key] = city;
+            return acc;
+        }, {})
+    }
+
+    //получаем name country
+    getCountryNameByCode(code) {
+        // this.countries содержит { 'Country-code': {...} }
+        return this.countries[code].name;
+    }
+
 
     //получение городов по коду страны
     getCitiesByCountryCode(code) {
@@ -30,3 +60,5 @@ class Locations {
 const locations = new Locations(api);
 
 export default locations;
+
+
